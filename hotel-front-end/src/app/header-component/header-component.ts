c74259eac6e2139c004647466732a8ecf5e973fd
Matthew Wright <matthew.wright9630@gmail.com>
2026-01-19 08:27:29 -0500

    Update Login

--- hotel-front-end/src/app/header-component/header-component.ts
+++ hotel-front-end/src/app/header-component/header-component.ts
@@ -18,7 +18,7 @@ export class HeaderComponent {
   }
 
   login() {
-    window.location.href = 'https://dun8rqxzjkgrc.cloudfront.net/oauth2/code/google';
+    window.location.href = '/oauth2/authorization/google';
   }
 
   getLoginDetails() {
